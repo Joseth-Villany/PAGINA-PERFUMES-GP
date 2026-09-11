9c723d73eb0038afe0edf47a66c0b19b9f6ce37d
Joseth-Villany <saenzjoseth@gmail.com>
2026-09-10 21:28:04 -0500

    primer push de la pagina de la perfumeria

---
+++ js/actions.js
@@ -0,0 +1,13 @@
+const searchForm = document.querySelector('.search-form');
+const searchBtn = document.querySelector('.search-btn');
+const searchInput = document.querySelector('.search-input');
+
+
+searchBtn.addEventListener('mouseenter', () => {
+  searchForm.classList.toggle('active');
+  searchInput.focus();
+});
+searchBtn.addEventListener("mouseleave" , () =>{
+    searchForm.classList.remove("active")
+    searchInput.blur()
+});
