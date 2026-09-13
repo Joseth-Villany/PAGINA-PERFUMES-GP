bf3622b602849b652c7eebe7d5119793b4f41984
Joseth-Villany <saenzjoseth@gmail.com>
2026-09-13 01:12:56 -0500

    cambios en la interfaz de inicio de la pagina (hover, nuevos botones, interaccion entre secciones de la interfaz)

--- js/actions.js
+++ js/actions.js
@@ -2,7 +2,6 @@ const searchForm = document.querySelector('.search-form');
 const searchBtn = document.querySelector('.search-btn');
 const searchInput = document.querySelector('.search-input');
 
-
 searchBtn.addEventListener('mouseenter', () => {
   searchForm.classList.toggle('active');
   searchInput.focus();
